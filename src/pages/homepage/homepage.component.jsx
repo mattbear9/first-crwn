1263ceab0b5de55c46d05375433ede4657ddaedd
unknown <mathieu.barthelemy@edu.esiee.fr>
2021-04-21 22:22:42 +0200

    update our menu-items to navigate wirthRouter HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -8,7 +8,7 @@ import Directory from '../../components/directory/directory.component';
 const HomePage = () => (
     <div className='homepage'>
 
-        <Directory />
+        <Directory/>
 
     </div>
 )
